d05616c661f6b64edf8f125a68e729dda31f483f
Tim Ruffles <timruffles@googlemail.com>
2012-02-05 15:37:51 +0000

    specify a window.authorised callback which takes authorised user as json

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_authorization_check
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
