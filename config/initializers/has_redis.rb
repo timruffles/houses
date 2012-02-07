b3bd68ae6ec0d8fa14138f41fa6917142ba5e15a
Tim Ruffles <timruffles@googlemail.com>
2012-02-07 14:44:52 +0000

    consts

--- config/initializers/has_redis.rb
+++ config/initializers/has_redis.rb
@@ -3,8 +3,8 @@ HasRedis.redis_instance = if ["production","staging"].include? ENV["RAILS_ENV"]
     uri = URI.parse(ENV["REDISTOGO_URL"])
     Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   rescue StandardError => e
-    stderr.puts "Error: no redis to go url? Got any error in any case, so no redis for you!"
-    stderr.puts e
+    STDERR.puts "Error: no redis to go url? Got any error in any case, so no redis for you!"
+    STDERR.puts e
   end
 else
   Redis.new
