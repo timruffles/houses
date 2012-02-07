a77bc8216e78a691867f943d7459edf5f57a4e65
Tim Ruffles <timruffles@googlemail.com>
2012-02-07 15:12:28 +0000

    well duh

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module TwitterMlServer
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_once_paths += %W(#{config.root}/lib)
     config.assets.initialize_on_precompile = false
-    config.assets.precompile += %w{app.js,app.css}
+    config.assets.precompile += %w{app.js app.css}
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
