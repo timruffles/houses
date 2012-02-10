5fefc60df4b2226b3304a7a959ed674be4500efe
Tim Ruffles <timruffles@googlemail.com>
2012-02-10 18:12:00 +0000

    this is y a break is requried

--- app/controllers/classified_tweets_controller.rb
+++ app/controllers/classified_tweets_controller.rb
@@ -1,5 +1,6 @@
 class ClassifiedTweetsController < ApplicationController
   authorize_resource
+  respond_to :json
   def update
     respond_with @classified_tweet = ClassifiedTweet.find_by_tweet_id!(params[:id])
   end
