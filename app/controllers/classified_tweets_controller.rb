c4246d0dfc66d4ccb5e17dfd113be78e56e3fe8c
Tim Ruffles <timruffles@googlemail.com>
2012-02-10 18:09:54 +0000

    end of the week bug

--- app/controllers/classified_tweets_controller.rb
+++ app/controllers/classified_tweets_controller.rb
@@ -1,6 +1,6 @@
 class ClassifiedTweetsController < ApplicationController
   authorize_resource
   def update
-    @classified_tweet = ClassifiedTweet.find_by_tweet_id! params[:id]
+    respond_with @classified_tweet = ClassifiedTweet.find_by_tweet_id!(params[:id])
   end
 end
