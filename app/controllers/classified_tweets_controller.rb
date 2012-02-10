879edab4d2073ee1e348e6fdf0b75998a3c9e308
Tim Ruffles <timruffles@googlemail.com>
2012-02-10 18:20:43 +0000

    o...

--- app/controllers/classified_tweets_controller.rb
+++ app/controllers/classified_tweets_controller.rb
@@ -2,6 +2,9 @@ class ClassifiedTweetsController < ApplicationController
   authorize_resource
   respond_to :json
   def update
-    respond_with @classified_tweet = ClassifiedTweet.find_by_tweet_id!(params[:id])
+    @classified_tweet = ClassifiedTweet.find_by_tweet_id!(params[:id])
+    @classified_tweet.category = params[:category]
+    @classified_tweet.save!
+    respond_with @classified_tweet
   end
 end
