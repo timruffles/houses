cb6e92efab48cb94102c1b79f4f6e4ddb8a3919d
Tim Ruffles <timruffles@googlemail.com>
2012-02-19 16:48:47 +0000

    two json reps for diff consumers

--- app/models/classified_tweet.rb
+++ app/models/classified_tweet.rb
@@ -1,17 +1,21 @@
 class ClassifiedTweet < ActiveRecord::Base
   belongs_to :tweet
   belongs_to :search
-  def as_json options = nil
-    json = (super).merge JSON.load(tweet.tweet)
-    json['id'] = json['id'].to_s
-    json
+  def as_json options = {}
+    if options[:for_node]
+      (super).merge 'tweet' => JSON.load(tweet.tweet)
+    else
+      json = (super).merge JSON.load(tweet.tweet) # we want tweet id
+      json['id'] = json['id'].to_s
+      json
+    end
   end
   include PublishesCallbacks
   after_create do
-    publish_callback :after_update, as_json
+    publish_callback :after_update, as_json(:for_node => true)
   end
   after_update do
     return unless category_changed?
-    publish_callback :after_update, as_json
+    publish_callback :after_update, as_json(:for_node => true)
   end
 end
