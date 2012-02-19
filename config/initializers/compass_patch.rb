c02924bd362be3dbbebede7e9b489b814015f60a
Tim Ruffles <timruffles@googlemail.com>
2012-02-19 21:10:38 +0000

    not needed in production

--- config/initializers/compass_patch.rb
+++ config/initializers/compass_patch.rb
@@ -1,44 +1,44 @@
+if ENV["RAILS_ENV"] != "production"
+  module Compass::SassExtensions::Functions::GradientSupport
 
-module Compass::SassExtensions::Functions::GradientSupport
+    class ColorStop < Sass::Script::Literal
 
-  class ColorStop < Sass::Script::Literal
-
-    def initialize(color, stop = nil)
-      self.options = {}
-      if color.is_a?(Sass::Script::String) && color.value == 'transparent'
-        color = Sass::Script::Color.new([0,0,0,0])
-        color.options = {}
-      end
-      unless Sass::Script::Color === color || Sass::Script::Funcall === color
-        raise Sass::SyntaxError, "Expected a color. Got: #{color}"
-      end
-      if stop && !stop.is_a?(Sass::Script::Number)
-        raise Sass::SyntaxError, "Expected a number. Got: #{stop}"
-      end
-      self.color, self.stop = color, stop
-    end
-
-  end
-
-  module Functions
-
-    def color_stops(*args)
-      Sass::Script::List.new(args.map do |arg|
-        case arg
-        when ColorStop
-          arg
-        when Sass::Script::Color
-          ColorStop.new(arg)
-        when Sass::Script::List
-          ColorStop.new(*arg.value)
-        when Sass::Script::String
-          ColorStop.new(arg)
-        else
-          raise Sass::SyntaxError, "Not a valid color stop: #{arg.class.name}: #{arg}"
+      def initialize(color, stop = nil)
+        self.options = {}
+        if color.is_a?(Sass::Script::String) && color.value == 'transparent'
+          color = Sass::Script::Color.new([0,0,0,0])
+          color.options = {}
         end
-      end, :comma)
+        unless Sass::Script::Color === color || Sass::Script::Funcall === color
+          raise Sass::SyntaxError, "Expected a color. Got: #{color}"
+        end
+        if stop && !stop.is_a?(Sass::Script::Number)
+          raise Sass::SyntaxError, "Expected a number. Got: #{stop}"
+        end
+        self.color, self.stop = color, stop
+      end
+
     end
 
+    module Functions
+
+      def color_stops(*args)
+        Sass::Script::List.new(args.map do |arg|
+          case arg
+          when ColorStop
+            arg
+          when Sass::Script::Color
+            ColorStop.new(arg)
+          when Sass::Script::List
+            ColorStop.new(*arg.value)
+          when Sass::Script::String
+            ColorStop.new(arg)
+          else
+            raise Sass::SyntaxError, "Not a valid color stop: #{arg.class.name}: #{arg}"
+          end
+        end, :comma)
+      end
+
+    end
   end
 end
-
