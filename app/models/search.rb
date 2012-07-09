afecc1ca124412eb6d2c4c0048a5010ee02b6c49
Tim Ruffles <tim@arachnys.com>
2012-07-09 09:18:19 +0100

    typo

--- app/models/search.rb
+++ app/models/search.rb
@@ -37,7 +37,7 @@ class Search < ActiveRecord::Base
   end
   after_destroy do
     publish_callback :after_destroy, {:keywords => keywords}
-    Resque.enqueue DelayedCall, Search.class.to_s, :clean_up, id.to_s
+    Resque.enqueue DelayedCall, Search.to_s, :clean_up, id.to_s
   end
 
 end
